f23363922d42628230d93f774e0e81c6a5daf33d
zhaoyanrong <1028508733@qq.com>
2020-06-18 16:27:29 +0800

    First Commit All

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemServiceApplication.java
@@ -0,0 +1,15 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ZhaoYR
+ * 2020/6/17 17:04
+ */
+@SpringBootApplication
+public class LeyouItemServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouItemServiceApplication.class,args);
+    }
+}
---
+++ leyou-registry/src/main/java/com/leyou/LeyouRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * ZhaoYR
+ * 2020/6/17 13:55
+ * 启动类
+ */
+public class LeyouRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouRegistryApplication.class);
+    }
+}
